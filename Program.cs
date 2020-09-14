0f6b2e47bd91047c6eac6df37038d91f9b77294c
Roberto-Livi <robertomlivi@outlook.com>
2020-09-14 15:40:32 -0400

    practicing formatting strings

--- Program.cs
+++ Program.cs
@@ -15,6 +15,21 @@ namespace csharpworld
             Calculator calculator = new Calculator();
             Console.WriteLine(calculator.Add(3, 4));
 
+            // Arrays
+            var numbers = new int[3] {1, 2, 3};
+            System.Console.WriteLine(numbers[2]);
+
+            var names = new string[2] {"Jack", "Jill"};
+            System.Console.WriteLine(names[0]);
+
+            // Strings
+            var firstName = "Rob";
+            string lastName = "Livi";
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+            var nms = new string[3] {"Jack", "John", "Mary"};
+            var formattedNames = string.Join(", ", nms);
+            System.Console.WriteLine(formattedNames);
+            
         }
     }
 }
