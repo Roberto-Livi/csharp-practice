04e22433dabc3654b4db2b91f840738a4f24e720
Roberto-Livi <robertomlivi@outlook.com>
2020-09-15 18:31:58 -0400

    added a conditional if statement depending on the hour

--- Program.cs
+++ Program.cs
@@ -65,6 +65,22 @@ namespace csharpworld
             MakeOld(person);
             System.Console.WriteLine(person.Age);
 
+            // if statemens
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                System.Console.WriteLine("It's morning");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                System.Console.WriteLine("Its afternoon");
+            }
+            else
+            {
+                System.Console.WriteLine("It's evening");
+            }
+
         }
 
         public static void Increment(int number){
