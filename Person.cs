9aab8357f69e20775e242346f5c852897075293f
Roberto-Livi <robertomlivi@outlook.com>
2020-09-15 16:55:29 -0400

    applied more reference types and values

--- Person.cs
+++ Person.cs
@@ -7,6 +7,8 @@ namespace csharpworld
         public string FirstName;
         public string LastName;
 
+        public int Age;
+
         public void Introduce() {
             Console.WriteLine("My name is {0} {1}", FirstName, LastName);
         }
--- Program.cs
+++ Program.cs
@@ -57,7 +57,22 @@ namespace csharpworld
             System.Console.WriteLine(array1[0]);
             System.Console.WriteLine(array2[0]);
 
+            var number = 1;
+            Increment(number);
+            System.Console.WriteLine(number);
 
+            var person = new Person() {Age = 20};
+            MakeOld(person);
+            System.Console.WriteLine(person.Age);
+
+        }
+
+        public static void Increment(int number){
+            number += 10;
+        }
+
+        public static void MakeOld(Person person){
+            person.Age += 10;
         }
     }
 }
